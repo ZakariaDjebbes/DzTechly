64c2071cda8c12db7837a631ce3c3e532b940d5a
Djebbes Zakaria <orochi255@hotmail.fr>
2021-04-21 22:23:00 +0200

    Setup of the mobile app

---
+++ Mobile/Mobile/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Mobile.ViewModels;
+using Xamarin.Forms;
+
+namespace Mobile.Views
+{
+	public partial class ItemDetailPage : ContentPage
+	{
+		public ItemDetailPage()
+		{
+			InitializeComponent();
+			BindingContext = new ItemDetailViewModel();
+		}
+	}
+}
---
+++ Mobile/Mobile/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Mobile.Models;
+using Mobile.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Mobile.Views
+{
+	public partial class NewItemPage : ContentPage
+	{
+		public Item Item { get; set; }
+
+		public NewItemPage()
+		{
+			InitializeComponent();
+			BindingContext = new NewItemViewModel();
+		}
+	}
+}
