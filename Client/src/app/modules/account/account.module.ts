40c383f2d405d4b0f60ee4ca5f7d830ebb9f0034
Djebbes Zakaria <orochi255@hotmail.fr>
2021-04-28 12:32:10 +0200

    Minor bugfix

--- Client/src/app/modules/account/account.module.ts
+++ Client/src/app/modules/account/account.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AccountComponent } from './account.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
 
 
 @NgModule({
-  declarations: [AccountComponent, LoginComponent, RegisterComponent],
+  declarations: [LoginComponent, RegisterComponent],
   imports: [
     CommonModule
   ]
